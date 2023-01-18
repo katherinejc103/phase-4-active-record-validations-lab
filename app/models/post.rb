d717fb3258cb4b20df057798d6707dab364dd7e3
Katherine <katherinejc103@gmail.com>
2023-01-17 21:44:29 -0500

    commit tree c9361f8ba2c3224803e9c65c64fb2c2de556809c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, acceptance: { accept: ["Fiction", "Non-Fiction"] } 
+    validate :clickbait
+
+    def clickbait
+         title.match?("True Facts")
+    end 
 end
